5572dd99860cbde597d8fb46358743be8463b6ec
James <47005482+WingsOfDivinity@users.noreply.github.com>
2024-12-20 22:54:34 -0700

    Update login-auth.js

--- login-auth.js
+++ login-auth.js
@@ -18,3 +18,33 @@ client.logOn({
 });
 
 
+// Event: logged in successfully 
+client.on('loggedOn', () => {
+    console.log("Logged into Steam successfully!");
+    client.setPersona(SteamUser.EPersonaState.Online);
+    console.log("Status set to Online.");
+
+    //Start idling
+    console.log(`Starting to idle game with App ID ${appID}...`);
+    client.gamesPlayed([appID]);
+    console.log(`Now idling game with App ID ${appID}...`);
+});
+
+// Event: Handle Steam Guard
+client.on('auth_code_required', (isEmail) => {
+    if (isEmail) {
+        console.log(`Steam Guard code sent to Email associated with username: ${username}`);
+    } else {
+        console.log(`Steam Guard code required from authenticator associated with username: ${username}`)
+    }
+    process.stdout.write("Enter Steam Guard code: "); // Prompt user for the code
+    process.stdin.once('data', (data) => {
+        client.provideAuthCode(data.toString().trim()); // Submit the code
+    });
+});
+
+// Event: Handle errors
+client.on('error', (err) => {
+    console.error(`An error occurred: ${err.message}`);
+});
+
