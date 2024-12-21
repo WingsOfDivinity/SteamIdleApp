09af21d210583c69ad4fcb7cef05e4f1d1b1d9d6
James <47005482+WingsOfDivinity@users.noreply.github.com>
2024-12-20 18:28:20 -0700

    Update login-auth.js

--- login-auth.js
+++ login-auth.js
@@ -7,3 +7,14 @@ const client = new SteamUser(); // create steamclient
 
 //login details
 const username = prompt("Enter Username: ");
+const password = prompt("Enter Password: ");
+const appID = prompt("Enter appID: ");
+
+//Logging in
+console.log("Attempting to log into Steam...");
+client.logOn({
+    accountName: username,
+    password: password,
+});
+
+
